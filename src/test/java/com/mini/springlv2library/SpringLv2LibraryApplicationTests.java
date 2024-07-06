dd6c395e2d72aa277c75623b13a1bc26576f975e
minyyyyyy <dpyo20020909@gmail.com>
2024-07-06 17:49:59 +0900

    first commit

---
+++ src/test/java/com/mini/springlv2library/SpringLv2LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mini.springlv2library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLv2LibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
